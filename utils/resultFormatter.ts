36eeca14587687f310239fce935434151ac16519
Himanshu Sharma <hs287190@gmail.com>
2025-04-21 15:29:27 +0530

    commented console logs used for debugging

--- utils/resultFormatter.ts
+++ utils/resultFormatter.ts
@@ -1,10 +1,10 @@
 export const resultFormatter = (result: number):string => {
-    console.log(`😀 ${result}`);
+    // console.log(`😀 ${result}`);
     
     if (result === null || result === -1) return "XX";
 
     if (result % 10 == result){
-        console.log(result);
+        // console.log(result);
         
         return "0" + result.toString();
     };
