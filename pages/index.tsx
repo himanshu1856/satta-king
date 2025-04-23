3bace37da4d3bb2ce40c20e78c847f90d610f7a5
Himanshu Sharma <hs287190@gmail.com>
2025-04-23 17:47:36 +0530

    favicon setting changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,7 +95,10 @@ export default function Home({
       <Head>
         <link rel="icon" href="/shiv.png" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/shiva.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/shiva.png"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/shiva.png"></link>
         <link rel="apple-touch-icon" href="/shiva.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
